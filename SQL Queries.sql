8b6ba82fd0c79140e26c4b0f9c7df1303d51f550
Giancarlo Sanna <G.Sanna@hotmail.de>
2023-01-03 21:52:35 +0100

    Added SQL EDA DA

--- SQL Queries.sql
+++ SQL Queries.sql
@@ -1,14 +1,50 @@
 use sports_betting;
 
-select w.* , i.med_income AS medium_income, s.online AS online, s.in_person AS inperson
-FROM wager w
-LEFT OUTER JOIN income i
-ON(w.year = i.year AND w.state = i.state)
-LEFT JOIN status s
-ON(w.state = s.state);
+CREATE TEMPORARY TABLE demographics
+SELECT p.*, i.med_income AS med_income
+FROM population p
+JOIN income i
+ON(p.year = i.year and p.state = i.state);
+CREATE TEMPORARY TABLE demographics2
+SELECT p.*, i.med_income AS med_income
+FROM population p
+JOIN income i
+ON(p.year = i.year and p.state = i.state);
 
+DROP TEMPORARY TABLE demographics;
+SELECT * FROM demographics;
+
+SELECT w.*, 
+d.population AS population, 
+d.poverty AS poverty,
+d.unemployed AS unemployed,
+d.med_income AS med_income,
+s.online AS online,
+s.in_person AS in_person
+FROM wager w
+LEFT JOIN demographics d
+ON(w.year = d.year and w.state = d.state)
+LEFT JOIN status s
+ON(s.state = w.state)
+
+UNION
+
+SELECT w2.*, 
+d2.population AS population, 
+d2.poverty AS poverty,
+d2.unemployed AS unemployed,
+d2.med_income AS med_income,
+s2.online AS online,
+s2.in_person AS in_person
+FROM wager w2
+RIGHT JOIN demographics2 d2
+ON(w2.year = d2.year and w2.state = d2.state)
+LEFT JOIN status s2
+ON(s2.state = d2.state);
 
 select * from income;
 select * from wager;
 select * from status;
 select * from population;
+
+
