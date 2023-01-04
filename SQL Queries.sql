8c185c6d7c3739e626f8c2d4093fc6d37c251e70
Giancarlo Sanna <G.Sanna@hotmail.de>
2023-01-04 12:56:50 +0100

    Safety Copy

--- SQL Queries.sql
+++ SQL Queries.sql
@@ -1,20 +1,13 @@
 use sports_betting;
 
-CREATE TEMPORARY TABLE demographics
-SELECT p.*, i.med_income AS med_income
-FROM population p
-JOIN income i
-ON(p.year = i.year and p.state = i.state);
-CREATE TEMPORARY TABLE demographics2
-SELECT p.*, i.med_income AS med_income
-FROM population p
-JOIN income i
-ON(p.year = i.year and p.state = i.state);
-
-DROP TEMPORARY TABLE demographics;
-SELECT * FROM demographics;
-
-SELECT w.*, 
+SELECT 
+w.year AS year,
+w.month AS month,
+w.state AS state,
+w.revenue AS revenue,
+w.handle AS handle,
+w.hold AS hold,
+w.taxes AS taxes,
 d.population AS population, 
 d.poverty AS poverty,
 d.unemployed AS unemployed,
@@ -29,7 +22,14 @@ ON(s.state = w.state)
 
 UNION
 
-SELECT w2.*, 
+SELECT 
+d2.year AS year,
+w2.month AS month,
+d2.state AS state,
+w2.revenue AS revenue,
+w2.handle AS handle,
+w2.hold AS hold,
+w2.taxes AS taxes,
 d2.population AS population, 
 d2.poverty AS poverty,
 d2.unemployed AS unemployed,
@@ -37,11 +37,13 @@ d2.med_income AS med_income,
 s2.online AS online,
 s2.in_person AS in_person
 FROM wager w2
-RIGHT JOIN demographics2 d2
+RIGHT JOIN demographics d2
 ON(w2.year = d2.year and w2.state = d2.state)
 LEFT JOIN status s2
 ON(s2.state = d2.state);
 
+
+SELECT * FROM demographics;
 select * from income;
 select * from wager;
 select * from status;
